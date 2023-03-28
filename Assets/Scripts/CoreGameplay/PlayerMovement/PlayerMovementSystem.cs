eeef38f0a23485f368ea737e887bca917114fe7c
APERF <a.perf.beta@gmail.com>
2023-03-28 12:15:24 -0300

    Camera Collision and Control
    Implemented Camera functions

--- Assets/Scripts/CoreGameplay/PlayerMovement/PlayerMovementSystem.cs
+++ Assets/Scripts/CoreGameplay/PlayerMovement/PlayerMovementSystem.cs
@@ -6,6 +6,10 @@ namespace CoreGameplay.PlayerMovement
 {
     public class PlayerMovementSystem : Singleton<PlayerMovementSystem>
     {
+        [SerializeField] private GameObject _mainCamera;
+        [SerializeField] private GameObject _target;
+        [SerializeField] private float _moveThreshold = 2f;
+
         public List<PlayerMover> Components;
 
         private void FixedUpdate()
@@ -18,7 +22,25 @@ namespace CoreGameplay.PlayerMovement
 
         public void Move(PlayerMover mover)
         {
-            mover.PlayerRb.velocity = new Vector3(mover.Direction.x, 0, mover.Direction.y) * mover.Speed;
+            float y = mover.PlayerRb.velocity.y;
+
+            Vector3 moveDirection = new Vector3(0, y, mover.Direction.y);
+            moveDirection = _mainCamera.transform.TransformDirection(moveDirection);
+            moveDirection.x += mover.Direction.x;
+            moveDirection.y = y;
+            moveDirection *= mover.Speed;
+            mover.PlayerRb.velocity = moveDirection;
+
+            Vector3 lookAtPos = _mainCamera.transform.forward * 10;
+            lookAtPos.y = mover.transform.position.y;
+            _target.transform.position = lookAtPos;
+            
+            if(moveDirection.magnitude * 10f > _moveThreshold)
+            {
+                Vector3 relativePos = _target.transform.position - mover.transform.position;
+                Quaternion newRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+                mover.transform.rotation = newRotation;
+            }
         }
     }
 }
